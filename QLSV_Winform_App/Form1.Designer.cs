7afee14a79c03551ee8c613d1f30882798b44e2d
Tiến Đặng Anh <20520800@ms.uit.edu.vn>
2022-05-17 10:39:03 +0700

    Add project files.

---
+++ QLSV_Winform_App/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace QLSV_Winform_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTenLop = new System.Windows.Forms.TextBox();
+            this.lbTenLop = new System.Windows.Forms.Label();
+            this.textBoxMaLop = new System.Windows.Forms.TextBox();
+            this.lbMaLop = new System.Windows.Forms.Label();
+            this.dgvLop = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.buttonSua = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MALOP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TENLOP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLop)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.txtTenLop);
+            this.groupBox1.Controls.Add(this.lbTenLop);
+            this.groupBox1.Controls.Add(this.textBoxMaLop);
+            this.groupBox1.Controls.Add(this.lbMaLop);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 101);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin lớp";
+            // 
+            // txtTenLop
+            // 
+            this.txtTenLop.Location = new System.Drawing.Point(434, 39);
+            this.txtTenLop.Name = "txtTenLop";
+            this.txtTenLop.Size = new System.Drawing.Size(253, 26);
+            this.txtTenLop.TabIndex = 3;
+            // 
+            // lbTenLop
+            // 
+            this.lbTenLop.AutoSize = true;
+            this.lbTenLop.Location = new System.Drawing.Point(361, 42);
+            this.lbTenLop.Name = "lbTenLop";
+            this.lbTenLop.Size = new System.Drawing.Size(67, 20);
+            this.lbTenLop.TabIndex = 2;
+            this.lbTenLop.Text = "Tên Lớp";
+            // 
+            // textBoxMaLop
+            // 
+            this.textBoxMaLop.Location = new System.Drawing.Point(107, 41);
+            this.textBoxMaLop.Name = "textBoxMaLop";
+            this.textBoxMaLop.Size = new System.Drawing.Size(138, 26);
+            this.textBoxMaLop.TabIndex = 1;
+            this.textBoxMaLop.TextChanged += new System.EventHandler(this.textBoxMaLop_TextChanged);
+            // 
+            // lbMaLop
+            // 
+            this.lbMaLop.AutoSize = true;
+            this.lbMaLop.Location = new System.Drawing.Point(38, 42);
+            this.lbMaLop.Name = "lbMaLop";
+            this.lbMaLop.Size = new System.Drawing.Size(62, 20);
+            this.lbMaLop.TabIndex = 0;
+            this.lbMaLop.Text = "Mã Lớp";
+            // 
+            // dgvLop
+            // 
+            this.dgvLop.AllowUserToAddRows = false;
+            this.dgvLop.AllowUserToDeleteRows = false;
+            this.dgvLop.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLop.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.MALOP,
+            this.TENLOP});
+            this.dgvLop.Location = new System.Drawing.Point(13, 133);
+            this.dgvLop.Name = "dgvLop";
+            this.dgvLop.ReadOnly = true;
+            this.dgvLop.Size = new System.Drawing.Size(724, 227);
+            this.dgvLop.TabIndex = 1;
+            this.dgvLop.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLop_RowEnter);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(3, 3);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(77, 32);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnThem);
+            this.flowLayoutPanel1.Controls.Add(this.btnXoa);
+            this.flowLayoutPanel1.Controls.Add(this.buttonSua);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 383);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(251, 39);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(86, 3);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(77, 32);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // buttonSua
+            // 
+            this.buttonSua.Location = new System.Drawing.Point(169, 3);
+            this.buttonSua.Name = "buttonSua";
+            this.buttonSua.Size = new System.Drawing.Size(77, 32);
+            this.buttonSua.TabIndex = 2;
+            this.buttonSua.Text = "Sửa";
+            this.buttonSua.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnExit.Location = new System.Drawing.Point(639, 382);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 36);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Visible = false;
+            // 
+            // MALOP
+            // 
+            this.MALOP.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.MALOP.DataPropertyName = "MALOP";
+            this.MALOP.HeaderText = "Mã Lớp";
+            this.MALOP.Name = "MALOP";
+            this.MALOP.ReadOnly = true;
+            this.MALOP.Width = 200;
+            // 
+            // TENLOP
+            // 
+            this.TENLOP.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TENLOP.DataPropertyName = "TENLOP";
+            this.TENLOP.HeaderText = "Tên Lớp";
+            this.TENLOP.Name = "TENLOP";
+            this.TENLOP.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(749, 449);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.dgvLop);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Student Management App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLop)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbMaLop;
+        private System.Windows.Forms.TextBox txtTenLop;
+        private System.Windows.Forms.Label lbTenLop;
+        private System.Windows.Forms.TextBox textBoxMaLop;
+        private System.Windows.Forms.DataGridView dgvLop;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button buttonSua;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MALOP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TENLOP;
+    }
+}
+
---
+++ QLSV_Winform_App/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QLSV_Winform_App
+{
+    public partial class Form1 : Form
+    {
+
+        string conStr = "Server=DESKTOP-GTJ76OT\\SQLEXPRESS;Database=LOP;Trusted_Connection=True;";
+        private SqlConnection con;
+        private SqlDataAdapter adapter;
+        private SqlCommand command;
+        private DataSet ds;
+        private DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Show(object sender, EventArgs e)
+        {
+            string sqlStr = "SELECT * FROM TENLOP";
+            adapter = new SqlDataAdapter(sqlStr, con);
+
+            ds = new DataSet();
+            adapter.Fill(ds, "TENLOP");
+            dt = ds.Tables["TENLOP"];
+
+            dgvLop.AutoGenerateColumns = false;
+            dgvLop.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(conStr);
+            con.Open();
+
+            Show(sender, e);
+
+            con.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (textBoxMaLop.Text == "" || textBoxMaLop.Text == null || txtTenLop.Text == "" || txtTenLop.Text == null)
+            {
+                MessageBox.Show("Khung đang trống, hãy chắc chắn rằng cả 2 khung đã được điền đầy đủ");
+            }
+            else
+            {
+                string queryString = "INSERT INTO TENLOP (MALOP,TENLOP) VALUES ('" + textBoxMaLop.Text + "', '" + txtTenLop.Text + "')";
+                using (SqlConnection connection = new SqlConnection(conStr))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                Show(sender, e);
+                textBoxMaLop.Text = "";
+                txtTenLop.Text = "";
+            }
+        }
+
+        private void dgvLop_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            textBoxMaLop.Text = dt.Rows[row]["MaLop"].ToString();
+            txtTenLop.Text = dt.Rows[row]["TenLop"].ToString();
+        }
+
+        private void textBoxMaLop_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void buttonSua_Click(object sender, EventArgs e)
+        //{
+        //    if (textBoxMaLop.Text == "" || textBoxMaLop.Text == null || txtTenLop.Text == "" || txtTenLop.Text == null)
+        //    {
+        //        MessageBox.Show("Khung đang trống, hãy chắc chắn rằng cả 2 khung đã được điền đầy đủ");
+        //    }
+        //    else
+        //    {
+        //        string queryString = "UPDATE TENLOP SET  ('" + textBoxMaLop.Text + "', '" + txtTenLop.Text + "')";
+        //        using (SqlConnection connection = new SqlConnection(conStr))
+        //        {
+        //            SqlCommand command = new SqlCommand(queryString, connection);
+        //            command.Connection.Open();
+        //            command.ExecuteNonQuery();
+        //        }
+        //        Show(sender, e);
+        //        textBoxMaLop.Text = "";
+        //        txtTenLop.Text = "";
+        //    }
+        //}
+    }
+}
